40d13ea9dce3094fc4f84c89054ef677a6523904
liammcluckie <liammcluckie@hotmail.co.uk>
2021-06-13 10:45:36 +0000

    Add home and register page route function

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
     
@@ -17,10 +18,20 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 @app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
 @app.route("/get_event")
 def get_event():
     event = mongo.db.event.find()
     return render_template("events.html", event=event)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
