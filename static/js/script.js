73c6bb8f9675c092640443343237599a74e7cdaf
liammcluckie <liammcluckie@hotmail.co.uk>
2021-06-24 15:05:16 +0000

    Update display card menu function syntax to jquery

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,5 @@
 // Display supper club card menu
 
-const menu = document.getElementById("event-menu");
-const menuBtn = document.getElementById("menu-button");
-
-menuBtn.onclick = function() {
-    if (menu.style.display !== "none") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-};
+$("#menu-button").click(function() {
+    $("#event-menu").toggle("400");
+})
