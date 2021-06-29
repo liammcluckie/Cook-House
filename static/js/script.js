3cf51b506c35c4dafc066db5d1870512e2ffa5ac
liammcluckie <liammcluckie@hotmail.co.uk>
2021-06-29 17:28:37 +0000

    Update and correct display card menu function

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,15 @@ document.querySelector("#copyright-year").innerText = new Date().getFullYear();
 
 // Display supper club card menu
 
-$("#menu-button").click(function() {
-    $("#event-menu").toggle("400");
-})
+document.querySelectorAll('.menu-btn').forEach(item => {
+    // Loop through all buttons and add click event listeners
+    item.addEventListener('click', () => {
+        // Check current display of elements next sibling
+        if (item.nextElementSibling.style.display === "none") {
+            // Change display style if true
+            item.nextElementSibling.style.display = "block";
+        } else {
+            item.nextElementSibling.style.display = "none";
+        }
+    });
+});
