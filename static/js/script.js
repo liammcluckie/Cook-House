f738d9d176c5f2629799c2571957e1279ea73989
liammcluckie <liammcluckie@hotmail.co.uk>
2021-07-01 16:46:52 +0000

    Update function to rotate caret icon

--- static/js/script.js
+++ static/js/script.js
@@ -2,16 +2,22 @@
 
 document.querySelector("#copyright-year").innerText = new Date().getFullYear();
 
-// Display supper club card menu
+/**
+ * Display supper club menu
+ * Rotate caret icon
+ */
 
 document.querySelectorAll('.menu-btn').forEach(item => {
     // Loop through all buttons and add click event listeners
     item.addEventListener('click', () => {
+        const caret = item.querySelector('.caret');
         // Check current display of elements next sibling
         if (item.nextElementSibling.style.display === "none") {
+            caret.classList.add('rotate');
             // Change display style if true
             item.nextElementSibling.style.display = "block";
         } else {
+            caret.classList.remove('rotate');
             item.nextElementSibling.style.display = "none";
         }
     });
